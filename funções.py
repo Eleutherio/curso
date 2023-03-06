5d914704eb1a5bdb633952a50132302c70f3aaa0
guifer em notebook <guifer.3097@@gmail.com>
2023-03-06 20:41:01 -0300

    save

--- funções.py
+++ funções.py
@@ -1,5 +1,22 @@
 #funções de construção
 
-https://docs.python.org/3.6/library/functions.html
+# https://docs.python.org/3.6/library/functions.html
+
+#Pandas
+
+import pandas as pd
+
+dataset = pd.read_csv(r'C:\Users\guife\Desktop\workspace\curso\data\db.csv', sep = ';')
+
+dt = dataset.get(['Nome', 'Quilometragem'])
+
+(zip(tuple(dt)))
+
+
+dt = pd.DataFrame(dt)
+
+dt = dt[['Quilometragem', 'Nome']]
+
+print(dt)
 
 
